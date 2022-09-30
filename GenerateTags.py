320b4a408456033eed585718535f1853cad9b292
Daniel Leong <56372780+DSLeong@users.noreply.github.com>
2022-09-30 14:16:50 +1000

    Add files via upload

--- GenerateTags.py
+++ GenerateTags.py
@@ -9,7 +9,7 @@ from utils import ARUCO_DICT
 
 
 class GenerateTags:
-    def __init__(self):
+    def __init__(self, tagType):
         #Find/Create Directory (Maybe place within GUI?)
         while True:
             try:
@@ -41,60 +41,9 @@ class GenerateTags:
                         dirpath = filedialog.askdirectory()
                         print(dirpath)
                         break
+                        
                     else:
                         print("ERROR")
-
-
-        #Type of ArUco Tag (Maybe have standard one e.g. DICT_5X5_100 or select of list)
-        good = False
-        while good == False:
-            try:
-                #https://docs.opencv.org/4.x/d9/d6a/group__aruco.html#gac84398a9ed9dd01306592dd616c2c975
-                print("ArUco tag List")
-                print("0: DICT_4X4_50  | 1: DICT_4X4_100  | 2: DICT_4X4_250  | 3: DICT_4X4_1000")
-                print("4: DICT_5X5_50  | 5: DICT_5X5_100  | 6: DICT_5X5_250  | 7: DICT_5X5_1000")
-                print("8: DICT_6X6_50  | 9: DICT_6X6_100  | 10: DICT_6X6_250 | 11: DICT_6X6_1000")
-                print("12: DICT_7X7_50 | 13: DICT_7X7_100 | 14: DICT_7X7_250 | 15: DICT_7X7_1000")
-                print("16: DICT_ARUCO_ORIGINAL | 17: DICT_APRILTAG_16h5 | 18: DICT_APRILTAG_25h9")
-                print("19: DICT_APRILTAG_36h10 | 20: DICT_APRILTAG_36h11\n")
-
-                print("Formating                | DICT_ARUCO_ORIGINAL = 6X6_1024")
-                print("DICT_5X5_100             | DICT_APRILTAG_16h5  = 4X4_30")
-                print("5x5 - pixel (internal)   | DICT_APRILTAG_25h9  = 5X5_35")
-                print("100 - Amount of id       | DICT_APRILTAG_36h10 = 6X6_2320")
-                print("                         | DICT_APRILTAG_25h9  = 6X6_587")
-
-                tagType = int(input("Please enter the type of ArUCo tag to generate: "))
-
-            except ValueError:
-                input("Please input Numeric Values.")
-            else:
-                if tagType < 0 or tagType > 20:
-                    input("Please input from Command List.")
-                else:
-                    if tagType == 0: tagType = "DICT_4X4_50"; range = 50;
-                    elif tagType == 1: tagType = "DICT_4X4_100"; range = 100;
-                    elif tagType == 2: tagType = "DICT_4X4_250"; range = 250;
-                    elif tagType == 3: tagType = "DICT_4X4_1000"; range = 100;
-                    elif tagType == 4: tagType = "DICT_5X5_50"; range = 50;
-                    elif tagType == 5: tagType = "DICT_5X5_100"; range = 100;
-                    elif tagType == 6: tagType = "DICT_5X5_250"; range = 250;
-                    elif tagType == 7: tagType = "DICT_5X5_1000"; range = 1000;
-                    elif tagType == 8: tagType = "DICT_6X6_50"; range = 50;
-                    elif tagType == 9: tagType = "DICT_6X6_100"; range = 100;
-                    elif tagType == 10: tagType = "DICT_6X6_250"; range = 250;
-                    elif tagType == 11: tagType = "DICT_6X6_1000"; range = 100;
-                    elif tagType == 12: tagType = "DICT_7X7_50"; range = 50;
-                    elif tagType == 13: tagType = "DICT_7X7_100"; range = 100;
-                    elif tagType == 14: tagType = "DICT_7X7_250"; range = 250;
-                    elif tagType == 15: tagType = "DICT_7X7_1000"; range = 1000;
-                    elif tagType == 16: tagType = "DICT_ARUCO_ORIGINAL"; range = 1024;
-                    elif tagType == 17: tagType = "DICT_APRILTAG_16h5"; range = 30;
-                    elif tagType == 18: tagType = "DICT_APRILTAG_25h9"; range = 35;
-                    elif tagType == 19: tagType = "DICT_APRILTAG_36h10"; range = 2320;
-                    elif tagType == 20: tagType = "DICT_APRILTAG_36h11"; range = 587;
-                    good = True
-
                
         #ID of ArUco Tag (Need to check if exists within type)
         good = False
--- PoseDetector.py
+++ PoseDetector.py
@@ -22,7 +22,9 @@ class PoseDetector:
         I = np.identity(3, dtype=R.dtype)
         n = np.linalg.norm(I - shouldBeIdentity)
         return n < 1e-6
-
+    
+    #=======================================================================================================================
+	#=======================================================================================================================
 
     #Convert Rotation 3x3 Matrix to euler Angles 
     def rotationMatrixToEulerAngles(self, R):
@@ -43,6 +45,9 @@ class PoseDetector:
         
         return np.array([x, y, z]) #returns pitch, roll and yaw (units?)
 
+
+    #=======================================================================================================================
+	#=======================================================================================================================
 
     #Display Pose
     def Display(self,x,y,z,ex,ey,ez):
@@ -67,9 +72,12 @@ class PoseDetector:
         print("===========================")
         print(" ")
 
+    #=======================================================================================================================
+	#=======================================================================================================================
+
 
     #Estimate Pose Values
-    def poseDetector(self, inputX, inputY, inputZ):
+    def poseDetector(self, inputX, inputY, inputZ, tagSetting, cameraList):
 
         #if Calibration does not exist
         if not os.path.isfile("calibration_matrix.npy") or not os.path.isfile("distortion_coefficients.npy"):
@@ -78,57 +86,8 @@ class PoseDetector:
             input("Press enter to continue")
 
         else:
-            #Multiple different markers?
-            good = False
-            while good == False:
-                try:
-                    #https://docs.opencv.org/4.x/d9/d6a/group__aruco.html#gac84398a9ed9dd01306592dd616c2c975
-                    print("ArUco tag List")
-                    print("0: DICT_4X4_50  | 1: DICT_4X4_100  | 2: DICT_4X4_250  | 3: DICT_4X4_1000")
-                    print("4: DICT_5X5_50  | 5: DICT_5X5_100  | 6: DICT_5X5_250  | 7: DICT_5X5_1000")
-                    print("8: DICT_6X6_50  | 9: DICT_6X6_100  | 10: DICT_6X6_250 | 11: DICT_6X6_1000")
-                    print("12: DICT_7X7_50 | 13: DICT_7X7_100 | 14: DICT_7X7_250 | 15: DICT_7X7_1000")
-                    print("16: DICT_ARUCO_ORIGINAL | 17: DICT_APRILTAG_16h5 | 18: DICT_APRILTAG_25h9")
-                    print("19: DICT_APRILTAG_36h10 | 20: DICT_APRILTAG_36h11\n")
-
-                    print("Formating                | DICT_ARUCO_ORIGINAL = 6X6_1024")
-                    print("DICT_5X5_100             | DICT_APRILTAG_16h5  = 4X4_30")
-                    print("5x5 - pixel (internal)   | DICT_APRILTAG_25h9  = 5X5_35")
-                    print("100 - Amount of id       | DICT_APRILTAG_36h10 = 6X6_2320")
-                    print("                         | DICT_APRILTAG_25h9  = 6X6_587")
-
-                    tagType = int(input("Please enter the type of ArUCo tag to detect: "))
-
-                except ValueError:
-                    input("Please input Numeric Values.")
-                else:
-                    if tagType < 0 or tagType > 20:
-                        input("Please input from Command List.")
-                    else:
-                        if tagType == 0: tagType = "DICT_4X4_50"
-                        elif tagType == 1: tagType = "DICT_4X4_100"
-                        elif tagType == 2: tagType = "DICT_4X4_250"
-                        elif tagType == 3: tagType = "DICT_4X4_1000"
-                        elif tagType == 4: tagType = "DICT_5X5_50"
-                        elif tagType == 5: tagType = "DICT_5X5_100"
-                        elif tagType == 6: tagType = "DICT_5X5_250"
-                        elif tagType == 7: tagType = "DICT_5X5_1000"
-                        elif tagType == 8: tagType = "DICT_6X6_50"
-                        elif tagType == 9: tagType = "DICT_6X6_100"
-                        elif tagType == 10: tagType = "DICT_6X6_250"
-                        elif tagType == 11: tagType = "DICT_6X6_1000"
-                        elif tagType == 12: tagType = "DICT_7X7_50"
-                        elif tagType == 13: tagType = "DICT_7X7_100"
-                        elif tagType == 14: tagType = "DICT_7X7_250"
-                        elif tagType == 15: tagType = "DICT_7X7_1000"
-                        elif tagType == 16: tagType = "DICT_ARUCO_ORIGINAL"
-                        elif tagType == 17: tagType = "DICT_APRILTAG_16h5"
-                        elif tagType == 18: tagType = "DICT_APRILTAG_25h9"
-                        elif tagType == 19: tagType = "DICT_APRILTAG_36h10"
-                        elif tagType == 20: tagType = "DICT_APRILTAG_36h11"
-                        good = True
-            
-            aruco_dict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[tagType])
+            #Set Tag Type
+            aruco_dict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[tagSetting["tagType"]])
 
             #Get Marker Size
             while True:
@@ -153,7 +112,10 @@ class PoseDetector:
                 follow = False
             
             #Check for camera (Usually 0)
-            cap = cv2.VideoCapture(0)
+            cap = cv2.VideoCapture(cameraList["index"])
+            cap.set(cv2.CAP_PROP_FRAME_WIDTH, cameraList["width"])
+            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cameraList["height"])
+            cap.set(cv2.CAP_FPS, cameraList["fps"])
 
             while True:
                 ret, frame = cap.read()
@@ -207,7 +169,7 @@ class PoseDetector:
                     tvec_str = "x=%4.0f y=%4.0f z=%4.0f eulerZ=%4.0f"%(x, y, z, math.degrees(eulerZ))
                     cv2.putText(frame, tvec_str, (20, 460), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2, cv2.LINE_AA)
                 
-                cv2.imshow("Image Feed - Press 'q' to stop", frame) 
+                cv2.imshow("Pose Estimation - Press 'q' to stop", frame) 
 
                 #Check for exit
                 key = cv2.waitKey(1) & 0xFF
